63ec0b8d563cac0a6a5a52cc8a539f432fdafb42
Ryan Holmes <holmes.ryan.m@gmail.com>
2020-04-15 14:06:09 +1000

    Add equatorial slice processing to standard processing.

--- Heat_Budget_Process_Production.m
+++ Heat_Budget_Process_Production.m
@@ -56,6 +56,7 @@ doMONANN   = 0; % 1 = calculate monthly and annually binned eulerian global budg
 doXYall    = 0; % 1 = do all XY calcs (most not used)
 doXYtran   = 0; % 1 = calculate vertically-integrated heat
                 % transports below given isotherm/s.
+doEQPM2    = 1; % 1 = calculate equatorial PM2-degree slice 
 
 % scaling constant on the transports:
 if (strcmp(model(1),'A')) %ACCESS-OM2, transport in kg/s
@@ -80,6 +81,7 @@ end
 hname = [base 'ocean_heat.nc'];
 if (strfind(baseD,'01'))
     fname = [base 'ocean_month.nc'];
+    m3name = [base 'ocean.nc'];
 else
     fname = [base 'ocean.nc'];
 end
@@ -98,6 +100,7 @@ yt = ncread(gname,'yt_ocean');yu = ncread(gname,'yu_ocean');
 
 % Vertical grid  -----------------------------------------
 z = ncread(fname,'st_ocean');zL = length(z);
+zw = ncread(fname,'sw_ocean');
 if (output ==0)
     % Initial dzt accounting for partial bottom cells:
     ht = ncread(gname,'ht');ze = ncread(fname,'st_edges_ocean');
@@ -1076,6 +1079,77 @@ for ti=1:tL
     save([outD model sprintf('_output%03d',output) '_' region '_ZAHBud.mat'],'ZA','yu','yt','-v7.3');
 end
 end % End doZA
+
+%%% Save longitude-depth slice of variables along equator:
+if (doEQPM2)
+% Equatorial Pacific:
+rname = 'EqPM2';
+[tmp lt1] = min(abs(lat(1,:)+2));
+[tmp lt2] = min(abs(lat(1,:)-2));
+[tmp ln1] = min(abs(lon(:,lt1)+240));
+[tmp ln2] = min(abs(lon(:,lt1)+70));
+
+temp = squeeze(mean(ncread(fname,'temp',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+if (strfind(baseD,'01'))
+    u = squeeze(mean(ncread(m3name,'u',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL 1]),2));
+    v = squeeze(mean(ncread(m3name,'v',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL 1]),2));
+else
+    u = squeeze(mean(ncread(fname,'u',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+    v = squeeze(mean(ncread(fname,'v',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+    try
+        u_sq = squeeze(mean(ncread(fname,'u_sq',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+        v_sq = squeeze(mean(ncread(fname,'v_sq',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+        uvsq = 1;
+    catch
+        uvsq = 0;
+    end
+end
+kappa = squeeze(mean(ncread(fname,'diff_cbt_t',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+taux = squeeze(mean(ncread(fname,'tau_x',[ln1 lt1 1],[ln2-ln1+1 lt2-lt1+1 tL]),2));
+tauy = squeeze(mean(ncread(fname,'tau_y',[ln1 lt1 1],[ln2-ln1+1 lt2-lt1+1 tL]),2));
+mld = squeeze(mean(ncread(fname,'mld',[ln1 lt1 1],[ln2-ln1+1 lt2-lt1+1 tL]),2));
+w = squeeze(mean(ncread(fname,'wt',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+try
+    w_sq = squeeze(mean(ncread(fname,'wt_sq',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+    Tdxsq = squeeze(mean(ncread(fname,'temp_dxsq',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+    Tdysq = squeeze(mean(ncread(fname,'temp_dysq',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+    Tdzsq = squeeze(mean(ncread(fname,'temp_dzsq',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 zL tL]),2));
+    wTsq = 1;
+catch
+    wTsq = 0;
+end
+
+% On isotherm terms:
+vdif = squeeze(mean(ncread(wname,'temp_vdiffuse_diff_cbt_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]),2));
+ndif = squeeze(mean(ncread(wname,'temp_numdiff_heat_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]),2));
+vnlc = squeeze(mean(ncread(wname,'temp_nonlocal_KPP_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]),2));
+pmer = squeeze(mean(ncread(wname,'sfc_hflux_pme_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]) + ...
+               ncread(wname,'temp_rivermix_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]),2));
+sufc = squeeze(mean(ncread(wname,'temp_vdiffuse_sbc_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]) + ...
+               ncread(wname,'frazil_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]) + ...
+               ncread(wname,'temp_eta_smooth_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]) + ...
+               ncread(wname,'sw_heat_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]),2));
+swrd = squeeze(mean(ncread(wname,'sw_heat_on_nrho',[ln1 lt1 1 1],[ln2-ln1+1 lt2-lt1+1 TL tL]),2));
+
+[Xt,Zt] = ndgrid(mean(lon(ln1:ln2,lt1:lt2),2),z);
+[Xu,Zu] = ndgrid(mean(lonu(ln1:ln2,lt1:lt2),2),z);
+[Xw,Zw] = ndgrid(mean(lon(ln1:ln2,lt1:lt2),2),zw);
+
+name = [outD 'mat_data/' model sprintf('_output%03d',output) ...
+        '_varsat_' rname '.mat']
+
+save([outD model sprintf('_output%03d',output) '_varsat_' rname '.mat'], ...
+     'Xt','Zt','Xu','Zu','Xw','Zw','temp','kappa', ...
+     'mld','taux','tauy', ...
+     'u','v','w','vdif','vnlc','pmer','sufc','swrd','ndif');
+if (uvsq)
+    save([outD model sprintf('_output%03d',output) '_varsat_' rname '.mat'],'u_sq','v_sq','-append');
+end
+if (wTsq)
+    save([outD model sprintf('_output%03d',output) '_varsat_' rname '.mat'],'w_sq','Tdxsq','Tdysq','Tdzsq','-append');
+end
+end % end doEQPM2
+
 
 
 % $$$ %% Zonally-averaged diffusivity (rough) --------------------------------------------------
